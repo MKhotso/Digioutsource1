4558a38cae7be03bfc95b40888a7438836d8ab16
Mkhot <Mkhot@SHUGGYRAY>
2024-04-11 15:13:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string digiName = "DiiigggiiiOuuuttttsssssouuuurcceee";
+        string newDigiName = RemoveDuplicates(digiName);
+        Console.WriteLine(newDigiName);
+    }
+
+    static string RemoveDuplicates(string str)
+    {
+        string result = string.Empty;
+        char previousChar = '\0'; // initialize to null character
+        foreach (char c in str)
+        {
+            if (c != previousChar)
+            {
+                result += c;
+                previousChar = c;
+            }
+        }
+        return result;
+    }
+}
